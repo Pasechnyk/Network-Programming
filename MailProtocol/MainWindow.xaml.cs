380decfc1753dd69b390664e7f023825f63b73da
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-06-13 23:38:08 +0300

    Add Mail Sender project

---
+++ MailProtocol/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailProtocol
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<User> users = new List<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void logInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var password = passwordTxt.Password;
+
+            if (string.IsNullOrWhiteSpace(loginTxt.Text) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Access denied: \n - fields are empty.");
+                return;
+            }
+            if (password == "gxknljmktrlthlyx")
+            {
+
+                MessageWindow form = new MessageWindow();
+
+                form.ShowDialog();
+                
+            }
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ MailProtocol/User.cs
@@ -0,0 +1,18 @@
+﻿namespace MailProtocol
+{
+    public partial class MainWindow
+    {
+        public class User
+        {
+            public int Id { get; set; }
+            public string Login { get; set; }
+            public string Password { get; set; }
+
+            public override string ToString()
+            {
+                return Login;
+            }
+
+        }
+    }
+}
