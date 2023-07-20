c6fcdd679db484e578fe4cb1e919906d2e351ffd
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-07-20 05:49:55 +0300

    Add files via upload

---
+++ OrderPizza/Collection/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Collection
+{
+    [Serializable]
+    public class Order
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Pizza { get; set; }
+    }
+}
